42bf8f0f4ac960f3bf881c0cf4fa69e677f06426
purushottam113 <shreepad113@gmail.com>
2025-05-09 16:24:59 +0530

    backend deploy config on netlify

---
+++ backend/netlify/functions/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/database');
+const authRouter = require('./routes/auth');
+const projectRouter = require('./routes/project');
+const taskRouter = require('./routes/task');
+const serverless = require('serverless-http');
+
+const PORT = process.env.PORT
+
+connectDB()
+    .then(()=> {
+        console.log("DB Connected");
+        app.listen(PORT, ()=> {
+            console.log("server running....");
+        })
+    })
+    .catch((err)=> {
+        console.error("DB not connected");
+    })
+
+app.use(cors({
+    // origin: "http://localhost:5173",
+    origin: "https://project-task-tracker.netlify.app",
+    credentials: true
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", authRouter);
+app.use("/", projectRouter);
+app.use("/", taskRouter);
+
+app.use("/", (req, res)=> {
+    res.send("HomePage on 3000")
+})
+
+module.exports.handler = serverless(app);
